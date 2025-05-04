4656adad9af633234ae15205601a226076a72947
𝐍𝐚𝐧𝐝 𝐘𝐚𝐝𝐮𝐰𝐚𝐧𝐬𝐡𝐢 <duhgfhfdghg+A1@gmail.com>
2025-05-04 15:52:47 +0530

    ShrutiBots

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/MesteriousPrivate/Micle",
+    "https://github.com/NoxxOP/NoxxMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
