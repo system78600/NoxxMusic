8abbc97250f11dc75c2848c7472219109cade698
system78600 <uzefkhatri552@gmail.com>
2025-05-12 22:55:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,10 +27,10 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", -1002543742001))
+LOGGER_ID = int(getenv("LOGGER_ID", -1002689253330))
 
 # Get this value from @HEROKU_CLUB on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 7903596664))
+OWNER_ID = int(getenv("OWNER_ID", 6020886539))
 
 
 ## Fill these variables if you're deploying on heroku.
@@ -48,8 +48,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+XkWpt5oqCuwwNWE1")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/system6o")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Cricket_Arenaaa")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/The_chillZzone")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
